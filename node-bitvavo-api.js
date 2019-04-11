aae8272830de470508e1caa176889a64cedca753
Bitvavo <joeri@bitvavo.com>
2019-04-11 10:22:09 +0200

    changed emitter error

--- node-bitvavo-api.js
+++ node-bitvavo-api.js
@@ -151,7 +151,7 @@ let api = function Bitvavo () {
     } else if ('authenticated' in response) {
       this.authenticated = true
     } else if ('action' in response) {
-      if (emitterReturned === false) {
+      if (emitterReturned === false && typeof keepLocalBookCopy === 'undefined') {
         errorToConsole('We have received a response, but you did not request the emitter in the right manner, please consult getting started with websockets in the readme.')
         return
       }
