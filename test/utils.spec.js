0dd89567e06623b57579a5a70bad9a52983cedea
Konstantin Möllers <ksm.moellers@gmail.com>
2023-11-20 13:13:04 +0100

    refactor: move isObject into utils

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,6 +1,23 @@
-const { sleep } = require('../lib/utils')
+const { isObject, sleep } = require('../lib/utils')
 
 describe('utils', () => {
+  describe('isObject', () => {
+    it('should return true for an object', () => {
+      expect(isObject({})).toBe(true)
+      expect(isObject(new Date())).toBe(true)
+      expect(isObject([])).toBe(true)
+    })
+
+    it('should return false for anything else', () => {
+      expect(isObject('str')).toBe(false)
+      expect(isObject(42)).toBe(false)
+      expect(isObject(null)).toBe(false)
+      expect(isObject(undefined)).toBe(false)
+      expect(isObject(true)).toBe(false)
+      expect(isObject(() => {})).toBe(false)
+    })
+  })
+
   describe('sleep', () => {
     beforeEach(() => {
       jest.useFakeTimers()
